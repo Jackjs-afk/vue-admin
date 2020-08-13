7b5a1e5832840062a522c6ac9d43ab0ae008ea37
Jackjs-afk <940798959@qq.com>
2020-08-13 15:30:11 +0800

    第一次提交

---
+++ src/views/Login/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="login">这里是登录页</div>
+</template>
+
+<script>
+export default {
+    name:'login'
+}
+</script>
+
+<style lang="scss" scoped>
+#login{
+    height: 100vh;
+    background: #344a5f;
+}
+</style>
